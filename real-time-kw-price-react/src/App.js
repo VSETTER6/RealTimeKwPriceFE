fc0a17e19e76465e898dedecff02c3b32002f3c8
VSETTER6 <vilhelm.settergren@live.se>
2025-01-19 20:18:40 +0100

    Home page added

--- real-time-kw-price-react/src/App.js
+++ real-time-kw-price-react/src/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import HomeContent from "./components/homeContent/HomeContent";
+import Main from "./components/main/Main";
 
 function App() {
   return (
-    <div className="App">
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <Main className="flex-grow">
+        <HomeContent />
+      </Main>
+      <Footer />
     </div>
   );
 }
